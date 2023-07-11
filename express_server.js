96834eedb287dde20340f835a45661de42c1a33b
vagrant <hassan.a.issak@gmail.com>
2023-07-11 03:37:32 +0000

    create input form and random string generator

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -30,7 +32,29 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]/* What goes here? */ };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = function(len) {
+  const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+  let randomArray = [];
+  for (let i = 0; i < len; i++) {
+    let randomIndex = Math.floor(Math.random() * (alphabetString.length - 1));
+    randomArray.push(alphabetString[randomIndex]);
+  }
+  let randomURL = randomArray.join('');
+  return randomURL;
+}
+
+
